d7929319842060c39cdbcd10c198c3f8b4cdfd05
Shaden Smith <shaden@cs.umn.edu>
2017-11-22 09:09:56 -0600

    figuring out best data structure to interface cmdline and csv

--- tensor_parser/cmd_args.py
+++ tensor_parser/cmd_args.py
@@ -1,12 +1,95 @@
 
 
+import sys
 import argparse
+from tensor_parser.index_map import index_map
+
+
+class csv_config:
+  '''
+    A class providing structure to the command-line arguments. This defines
+    tensor modes, their respective fields in the CSV file(s), etc.
+  '''
+
+  def __init__(self, cmd_args):
+    self._inputs = cmd_args.csv
+    self._output = cmd_args.tensor
+    self._field_sep = cmd_args.field_sep
+
+    self._modes = []
+    if cmd_args.field is None:
+      print("ERROR: no fields from CSV specified. Re-run with '--help'")
+      sys.exit(1)
+
+    # construct mode dictionaries
+    for f in cmd_args.field:
+      self._modes.append(
+        {
+          'csv_field' : f,
+          'sort'      : index_map.SORT_NONE,
+        }
+      )
+
+    # Set non-default sort values
+    if cmd_args.sort_num:
+      for field_name in cmd_args.sort_num:
+        for x in self._modes:
+          if x['csv_field'] == field_name:
+            x['sort'] = index_map.SORT_NUM
+    if cmd_args.sort_lex:
+      for field_name in cmd_args.sort_lex:
+        for x in self._modes:
+          if x['csv_field'] == field_name:
+            x['sort'] = index_map.SORT_LEX
+
+
+
+  def mode(self, mode_idx):
+    '''
+      Return the dictionary representing meta-data for mode `mode_idx`. The
+      dictionary will take the form:
+      {
+        csv_field => one of the columns in the CSV file
+        sort => one of the appropriate `index_map.SORT_XXX` values
+      }
+    '''
+    return self._modes[mode_idx]
+
+  def delimiter(self):
+    '''
+      User-specified CSV delimiter. `None` if unspecified.
+    '''
+    return self._field_sep
+
+  def inputs(self):
+    '''
+      Return the list of input CSV files.
+    '''
+    return self._inputs
+
+  def output(self):
+    '''
+      The output filename.
+    '''
+    return self._output
+
+
+  def num_modes(self):
+    '''
+      Return the number of modes in the tensor.
+    '''
+    return len(self._modes)
+
 
 
 def parse_args(cmd_args=None):
   my_description = '''
     Construct a tensor from CSV-like files. The files can either be in plain
     text form or compressed (.gz or .bz2).
+
+    Fields can be specified by their name if the CSV file has a header, or
+    otherwise can be specified with a 1-indexed integer corresponding to their
+    column in the CSV file.
 
     If no field is provided for tensor values ('--vals'), then a binary tensor
     is constructed.
@@ -43,8 +126,8 @@ def parse_args(cmd_args=None):
 
   if cmd_args is not None:
     # parse provided args
-    return parser.parse_args(cmd_args)
+    return csv_config(parser.parse_args(cmd_args))
   else:
     # parse sys.argv
-    return parser.parse_args()
+    return csv_config(parser.parse_args())
 
--- tests/test_cmd_args.py
+++ tests/test_cmd_args.py
@@ -4,47 +4,38 @@ import argparse
 
 import tests
 from tensor_parser import cmd_args
+from tensor_parser.index_map import index_map
 
 class TestCSVParser(unittest.TestCase):
 
   def test_positionals(self):
-    myargs = ['1.csv', '2.csv', 'out.tns']
-    args = cmd_args.parse_args(myargs)
-    self.assertEqual(len(args.csv), 2)
-    self.assertEqual(args.csv[0], '1.csv')
-    self.assertEqual(args.csv[1], '2.csv')
-    self.assertEqual(args.tensor, 'out.tns')
+    myargs = ['1.csv', '2.csv', 'out.tns', '-f1']
+    config = cmd_args.parse_args(myargs)
+    self.assertEqual(len(config.inputs()), 2)
+    self.assertEqual(config.inputs()[0], '1.csv')
+    self.assertEqual(config.inputs()[1], '2.csv')
+    self.assertEqual(config.output(), 'out.tns')
 
   def test_delim(self):
-    myargs = ['1.csv', '2.csv', 'out.tns', '-F|']
-    args = cmd_args.parse_args(myargs)
-    self.assertEqual(args.field_sep, '|')
+    myargs = ['1.csv', '2.csv', 'out.tns', '-F|', '-f1']
+    config = cmd_args.parse_args(myargs)
+    self.assertEqual(config.delimiter(), '|')
 
-  def test_delim(self):
-    myargs = ['1.csv', '2.csv', 'out.tns', '-f2', '-fname']
-    args = cmd_args.parse_args(myargs)
-    self.assertEqual(len(args.field), 2)
-    self.assertEqual(args.field[0], '2')
-    self.assertEqual(args.field[1], 'name')
+  def test_fields(self):
+    myargs = ['1.csv', '2.csv', 'out.tns', '-f2', '-fuser']
+    config = cmd_args.parse_args(myargs)
+    self.assertEqual(config.num_modes(), 2)
+    self.assertEqual(config.mode(0)['csv_field'], '2')
+    self.assertEqual(config.mode(1)['csv_field'], 'user')
+    self.assertEqual(config.mode(0)['sort'], index_map.SORT_NONE)
+    self.assertEqual(config.mode(1)['sort'], index_map.SORT_NONE)
 
   def test_sort(self):
-    myargs = ['1.csv', '2.csv', 'out.tns', '-f2', '-fname', '-n2', '-lname']
-    args = cmd_args.parse_args(myargs)
-    self.assertEqual(len(args.sort_num), 1)
-    self.assertEqual(args.sort_num[0], '2')
-    self.assertEqual(len(args.sort_lex), 1)
-    self.assertEqual(args.sort_lex[0], 'name')
+    myargs = ['1.csv', '2.csv', 'out.tns', '-f2', '-fuser', '-n2', '-luser']
+    config = cmd_args.parse_args(myargs)
+    self.assertEqual(config.mode(0)['sort'], index_map.SORT_NUM)
+    self.assertEqual(config.mode(1)['sort'], index_map.SORT_LEX)
 
-  '''
-  # Commented out because failed argparse prints to stderr, which is a pain...
-  def test_missing(self):
-    myargs = ['1.csv']
-    try:
-      args = cmd_args.parse_args(myargs)
-    except:
-      pass
-  '''
-    
 
 if __name__ == '__main__':
     unittest.main()
